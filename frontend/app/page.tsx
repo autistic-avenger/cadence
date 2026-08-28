8fe47afedd6d49ff8205c6cf6485728f3ae345e3
Aditya Kumar <aditya4u164@gmail.com>
2026-08-28 18:42:27 +0530

    feat:layout setup

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,16 @@
+import Navbar from "@/ui/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <></>
+    <div>
+      <Navbar/>
+      <div className="relative w-full min-h-screen flex">
+        
+        <div className="absolute bottom-0 w-full h-[50%] bg-linear-to-t from-white from-15% to-transparent z-2"></div>
+
+        <img src="/hero_bg.jpg" alt="hero-bg" className="object-cover select-none drag [-webkit-user-drag:none]"/>      
+      </div>
+    </div>
   );
 }
---
+++ frontend/ui/components/Navbar.tsx
@@ -0,0 +1,17 @@
+
+export default function Navbar() {
+  return (
+    <div className="fixed flex justify-center items-center z-50 top-3 w-full h-15 "> 
+        <div className="flex pl-5 items-center justify-between h-13 w-255 rounded-2xl bg-white/35 shadow-2xs p-2 backdrop-blur-sm mx-2">
+            <div>
+
+            </div>
+
+            <div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
